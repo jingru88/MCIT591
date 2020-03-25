3b0a608248cf3bd24f5d5144580899b6224e97ea
jingru88 <jingru88@seas.upenn.edu>
2020-03-24 17:28:44 -1000

    Adding Person.java and gitignore

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+
+public class Person {
+String name;
+int age;
+
+Person (String n, int a){
+	name = n;
+	age = a;
+}
+
+public static void main (String []args) {
+	Person p = new Person("Bob", 50);
+	System.out.println(p.name);
+}
+}
